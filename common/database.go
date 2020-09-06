106e0b1fbe5888ffee8420c2c21b90cb979ca903
ohmyray <ohmyray@163.com>
2020-09-07 00:50:54 +0800

    yml配置文件化

--- common/database.go
+++ common/database.go
@@ -4,18 +4,19 @@ import (
 	"fmt"
 	"github.com/jinzhu/gorm"
 	"github.com/ohmyray/gin-demo01/model"
+	"github.com/spf13/viper"
 )
 
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	driverName := "mysql"
-	host := "localhost"
-	port := "3306"
-	database := "gindemo01"
-	charset := "utf8"
-	username := "root"
-	password := "root"
+	driverName := viper.GetString("dataSource.driverName")
+	host := viper.GetString("dataSource.host")
+	port := viper.GetString("dataSource.port")
+	database := viper.GetString("dataSource.database")
+	charset := viper.GetString("dataSource.charset")
+	username := viper.GetString("dataSource.username")
+	password := viper.GetString("dataSource.password")
 	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
 		username, password, host, port, database, charset)
 
--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/ohmyray/gin-demo01/common"
+	"github.com/ohmyray/gin-demo01/config"
 	"github.com/ohmyray/gin-demo01/router"
+	"github.com/spf13/viper"
+	"log"
 )
 
-
 func main() {
+
+	// 获取配置信息
+	config.InitConfig()
+
 	// 获取数据库连接
 	db := common.InitDB()
 	defer db.Close()
@@ -16,5 +22,10 @@ func main() {
 	r := gin.Default()
 	r = router.CollectRoute(r)
 
+	serverPort := viper.GetString("server.port")
+	log.Printf("Server run at http://localhost:%v", serverPort)
+	if serverPort != "" {
+		panic(r.Run(":" + serverPort))
+	}
 	panic(r.Run())
 }
